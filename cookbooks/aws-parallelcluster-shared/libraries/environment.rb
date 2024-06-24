a0c5d5f9b600c41f37011b0ec6450bce6481e0e3
hardeep <hardeep@pacificfusion.com>
2024-06-24 23:56:23 +0000

    first commit

---
+++ cookbooks/aws-parallelcluster-shared/libraries/environment.rb
@@ -0,0 +1,72 @@
+def aws_region
+  node['cluster']['region']
+end
+
+def aws_domain
+  # Get the aws domain name
+  region = aws_region
+  if region.start_with?("cn-")
+    "amazonaws.com.cn"
+  elsif region.start_with?("us-iso-")
+    "c2s.ic.gov"
+  elsif region.start_with?("us-isob-")
+    "sc2s.sgov.gov"
+  else
+    "amazonaws.com"
+  end
+end
+
+# Virtual Environments
+def virtualenv_path(pyenv_root:, python_version:, virtualenv_name:)
+  "#{pyenv_root}/versions/#{python_version}/envs/#{virtualenv_name}"
+end
+
+def cookbook_virtualenv_name
+  'cookbook_virtualenv'
+end
+
+def cookbook_python_version
+  node['cluster']['python-version']
+end
+
+def cookbook_pyenv_root
+  node['cluster']['system_pyenv_root']
+end
+
+def cookbook_virtualenv_path
+  virtualenv_path(pyenv_root: cookbook_pyenv_root, python_version: cookbook_python_version, virtualenv_name: cookbook_virtualenv_name)
+end
+
+def node_virtualenv_name
+  'node_virtualenv'
+end
+
+def node_python_version
+  node['cluster']['python-version']
+end
+
+def node_pyenv_root
+  node['cluster']['system_pyenv_root']
+end
+
+def node_virtualenv_path
+  virtualenv_path(pyenv_root: node_pyenv_root, python_version: node_python_version, virtualenv_name: node_virtualenv_name)
+end
+
+#
+# Check if this is an ARM instance
+#
+def arm_instance?
+  node['kernel']['machine'] == 'aarch64'
+end
+
+def x86_instance?
+  node['kernel']['machine'] == 'x86_64'
+end
+
+#
+# Check if DCV is installed
+#
+def dcv_installed?
+  ::File.exist?("/etc/dcv/dcv.conf")
+end
---
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -0,0 +1,10 @@
+# Slurm
+default['cluster']['slurm']['version'] = '23-11-4-1'
+default['cluster']['slurm']['commit'] = ''
+default['cluster']['slurm']['branch'] = ''
+default['cluster']['slurm']['sha256'] = '832bc076d8ac9fd44cf2bf539c5046d7aa3ec20fb6c699693552dce3e5ad5588'
+default['cluster']['slurm']['base_url'] = "https://github.com/SchedMD/slurm/archive"
+# Munge
+default['cluster']['munge']['munge_version'] = '0.5.15'
+default['cluster']['munge']['sha256'] = '51b2c81d1a7ec2ab5d486fa51b50c7e79eb1810ca6687b6ed65f3601abc55614'
+default['cluster']['munge']['base_url'] = "https://github.com/dun/munge/archive"
